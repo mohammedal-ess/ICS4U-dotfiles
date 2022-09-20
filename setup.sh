96539d6ede7dba3d79dc396ebfd6e2f968a8834c
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2022-09-20 09:21:39 -0400

    add setup

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+#
+# On  a new AWS Debian Buster EC2 Instance you will have to do the following before running this 
+#
+# sudo apt update
+# sudo apt install -y git
+# 
+# then ...
+# git clone https://github.com/mohammedal-ess/ICS4U-dotfiles.git
+# cd .dotfiles.git && sh ./setup.sh
+#
+# update and upgrade system 
+echo Update and upgrade system
+sudo apt-get update
+sudo apt-get full-upgrade -y
+
+# Configure Git
+git config --global user.email "mohammed.al-essawi@mths.ca"
+git config --global user.email "mohammed.al-essawi"
+git config --global init.defaultBranch main
+git config -- global pull.rebase true
+git config -- global fetch.rebase true
