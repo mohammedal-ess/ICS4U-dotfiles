4cd28b6731a3d86db2baf7d2e4361d5542fd7f4f
mohammed al-essawi <mohammed.al-essawi@mths.ca>
2022-09-22 13:40:35 +0000

    init

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,12 @@
+vim.opt.encoding = "utf-8"
+
+vim.opt.number = true 
+vim.opt.relativenumber = true
+vim.opt.cursorline = true 
+vim.opt.numberwidth = 4
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true 
+
+vim.opt.scrolloff = 6
+vim.opt.wrap = false
